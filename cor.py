85f63e5187fbf467e6175705fa7c9093bf944c0b
Snoeprol <mariovanrooij@hotmail.com>
2020-09-23 19:49:06 +0200

    Delete cor.py

--- cor.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy
-
-for i in range(100):
-    x = numpy.random.multivariate_normal([0,0], [[4,150],[150,1]])
-    plt.plot(x[0], x[1], '.')
-
-
-plt.show()
